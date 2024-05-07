17a1b227130aad308cfe6f9c7758444795761101
Washim-RaJa <washimrajam75@gmail.com>
2024-05-07 22:11:21 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route, } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
 
 function App() {
 
   return (
-    <>
-     <h1 className="text-5xl bg-green-400 p-[20%]">Bismillah !</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
