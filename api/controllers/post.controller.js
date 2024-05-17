0ff6a3f67e4c7b43a5d6e9e593f150878085806d
Washim-RaJa <washimrajam75@gmail.com>
2024-05-17 22:37:46 +0530

    Created get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -23,3 +23,45 @@ export const createPost = async (req, res, next)=> {
         next(error)
     }
 }
+
+export const getPosts = async (req, res, next)=> {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === 'asc' ? 1 : -1;
+        const posts = await Post.find({
+            
+            ...(req.query.userId && { userId: req.query.userId }),          // if req.query has a userId then serach by userId
+            ...(req.query.category && { category: req.query.category }),    // if req.query has a category then serach by category
+            ...(req.query.slug && { slug: req.query.slug }),                // if req.query has a slug then serach by slug
+            ...(req.query.postId && { _id: req.query.postId }),             // if req.query has a postId then serach by postId
+            
+            ...(req.query.searchTerm && {   // it will search in both title & content by the given searchTerm
+                $or: [ // It allows searching in both title & content
+                    { title: { $regex: req.query.searchTerm, $options: 'i' } }, // $regex search inside the title & i means case insensitive
+                    { content: { $regex: req.query.searchTerm, $options: 'i' } }
+                ],
+            }),
+        }).sort({ updatedAt: sortDirection})
+          .skip(startIndex)
+          .limit(limit);
+
+        const totalPosts = await Post.countDocuments();
+
+        const now = new Date();
+
+        const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() - 1 , now.getDate());
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: { $gte: oneMonthAgo }
+        });
+
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMonthPosts
+        });
+        
+    } catch (error) {
+        next(error)
+    }
+}
