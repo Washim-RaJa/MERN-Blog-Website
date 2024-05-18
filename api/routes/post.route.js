e154799076103717b5f2b3d465d399285f6e20ad
Washim-RaJa <washimrajam75@gmail.com>
2024-05-18 15:52:45 +0530

    Added delete post functionality to the DashPost

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { createPost, getPosts } from '../controllers/post.controller.js';
+import { createPost, deletePosts, getPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createPost);
 router.get('/getposts', getPosts);
+router.delete('/deletepost/:postId/:userId',verifyToken, deletePosts);
 
 export default router;
