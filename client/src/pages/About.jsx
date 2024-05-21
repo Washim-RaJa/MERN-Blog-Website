24a98535cbb85d7335c749669c675ac4f250061f
Washim-RaJa <washimrajam75@gmail.com>
2024-05-22 02:17:53 +0530

    Completed About and Projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,23 @@
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">About RaJa&apos;s Blog</h1>
+          <div className="text-base text-gray-500 flex flex-col gap-6">
+            <p className="">
+              RaJa&apos;s Blog is a blog that I created to share my thoughts and ideas with the world. I am a software engineer and I love to write about my experiences and things that I have learned. I hope enjoy reading my blog.
+            </p>
+            <p className="">
+              On this blog, you&apos;ll find weekly articles and tutorials on topics such as web development, software engineering, and programming languages. RaJa is always learning and exploring new technologies, so be sure to check back often for new content!
+            </p>
+            <p className="">
+              We encourage you to leave comments on our posts and engage with other readers. You can like other people&apos;s comments and reply to them as well. We believe that a community of learners can help each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
